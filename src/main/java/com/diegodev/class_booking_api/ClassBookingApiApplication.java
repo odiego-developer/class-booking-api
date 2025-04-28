d4002bb18fc4d3d903f45aaaf4ed6e80375e3d4f
d.silva4 <d.silva@tech-lab.io>
2025-04-28 16:57:49 -0300

    feat: create main class as well as the application.yml for the project

---
+++ src/main/java/com/diegodev/class_booking_api/ClassBookingApiApplication.java
@@ -0,0 +1,12 @@
+package com.diegodev.class_booking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassBookingApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ClassBookingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diegodev/class_booking_api/ClassBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegodev.class_booking_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassBookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
